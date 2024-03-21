f7e45b61a9be81fd5680aa588c72647e08be1812
Hamza Najfi <141533696+asli-nobita@users.noreply.github.com>
2024-03-21 23:49:18 +0530

    Isoline template

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const center = { 
+   lat: 52.5159, 
+   lng: 13.3777, 
+   text: 'Berlin, Germany'
+}
+
+const hereCredentials = {
+   id: 'QJduefuzyCbiEWviBdtj',
+   code: 'YOUR-HERE-CODE',
+   apikey: 'nlyxv5Ld270xr0N8HARuYHa9sccBN04ZiGXs0eAHEf0'
+}
+
+export { center, hereCredentials };
